224291f5686e5b2a4be746d4a72d427ba4bf8c76
p111111111uto <p1anet9ine@pm.me>
2026-03-16 17:01:49 -0400

    Refactor bot implementation to use commands.Bot and add logging functionality

--- bot.py
+++ bot.py
@@ -1,23 +1,32 @@
 import discord
 from discord.ext import commands
+import logging
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8'm mode='w')
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
 bot = commands.Bot(command_prefix='/', intents=intents)
 
-@bot.command()
-async def test(ctx):
-    pass
+@bot.event()
+async def on_ready():
+    print(f'Bot is ready.')
 
-client = MyClient(intents=intents)
-client.run(DISCORD_TOKEN)
+@bot.event()
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    
+    await bot.process_commands(message)
+
+@bot.command()
+async def guess(ctx):
+    await ctx.send('Message received')
+
+bot.run(DISCORD_TOKEN, log_handler=handler, log_level=logging.DEBUG)
