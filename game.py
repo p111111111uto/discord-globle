7086f82ef528f36a06c7f1851269f2427515c218
p111111111uto <p1anet9ine@pm.me>
2026-03-17 09:16:18 -0400

    Refactor game.py to improve code clarity and update Earth's radius in haversine function as well as adding proximity_percentage function

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import datetime
 import random
 import math
 
+# Loads CSV into a list of dictionaries
 def load_countries():
     countries_list = []
     with open('countries.csv', newline='') as csvfile:
@@ -10,12 +11,14 @@ def load_countries():
         for row in reader:
             countries_list.append(row)
         return countries_list
-    
+
+# Seeded daily country pick
 def daily_country(countries_list):
     date_as_seed = int(datetime.date.today().strftime('%Y%m%d'))
     random.seed(date_as_seed)
     return random.choice(countries_list)
 
+# Case-insensitive country lookup
 def find_country(name, countries_list):
 
     for country in countries_list:
@@ -23,6 +26,7 @@ def find_country(name, countries_list):
             return country
     return None
 
+# Distance in KM
 def haversine(lat1, lon1, lat2, lon2):
     lat1 = math.radians(lat1)
     lon1 = math.radians(lon1)
@@ -37,7 +41,15 @@ def haversine(lat1, lon1, lat2, lon2):
     a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
     
-    # Earth's radius
-    R = 6371
+    # Earth's radius in miles
+    R = 3958.8
 
-    return R * c
+    return round(R * c,2)
+
+# 0 - 100 score
+def proximity_percent(distance):
+    # Half of Earth's circumference in miles
+    MAX_DIST = 12451
+    # Proximity formula
+    proximity = max(0, (100 - (distance / MAX_DIST * 100)))
+    return int(proximity)
